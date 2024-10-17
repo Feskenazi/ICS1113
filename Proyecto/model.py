fd6b1360c82311c92c2194e47d3a7bc022bb31ce
Feskenazi <100655059+Feskenazi@users.noreply.github.com>
2024-10-17 20:21:04 -0300

    Update model.py

--- Proyecto/model.py
+++ Proyecto/model.py
@@ -1,1 +1,13 @@
--
+#----------------------- GUROBI ------------------------
+import gurobipy as gp
+from gurobipy import GRB
+import pandas as pd
+from parameters import get_data
+# from plot import plot_solution
+# import os
+
+
+#----------------------- Generacion del modelo ------------------------
+
+
+
