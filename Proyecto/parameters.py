65feda84c6c0a359ae8bb839c6e7e737abf86a65
Feskenazi <100655059+Feskenazi@users.noreply.github.com>
2024-10-17 19:59:28 -0300

    Update parameters.py

--- Proyecto/parameters.py
+++ Proyecto/parameters.py
@@ -1,1 +1,13 @@
--
+import pandas as pd
+# import random (si queremos añadir ruido)
+# random.seed(0)
+
+def get_data(csv_type):
+  
+  if csv_type == "small":
+
+    #BASE DE DATOS PEQUEÑA
+    
+
+
+
