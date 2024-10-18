279f3adc631f01409b231490915340bdc9c91a05
Feskenazi <100655059+Feskenazi@users.noreply.github.com>
2024-10-17 22:17:56 -0300

    Update parameters.py

--- Proyecto/parameters.py
+++ Proyecto/parameters.py
@@ -32,9 +32,9 @@ def get_data(csv_type):
   E = list(range(len(cos_hor)))
   S = list(range(len(des_sit)))
   D = list(range(len(des_sit.columns)))
-  TH = set(range(24))
-  TD = set(range(7))
-  TS = set(range(4))
+  TH = list(range(24))
+  TD = list(range(7))
+  TS = list(range(4))
   
   # Crear parámetros
   
