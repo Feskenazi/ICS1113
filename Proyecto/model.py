798f01d06e83bc00a6a0264e0c8ea8f17a97a5b5
Feskenazi <100655059+Feskenazi@users.noreply.github.com>
2024-10-18 23:59:21 -0300

    Update model.py

--- Proyecto/model.py
+++ Proyecto/model.py
@@ -8,24 +8,24 @@ from parameters import get_data
 
 def generate_model(PARAMETERS):
 
-  #Obtener los datos desde parameters.py
-  E, S, TH, TD, TS, CO_s, CD_es, CH_e, TR_es, EP_es = PARAMETERS
+    #Obtener los datos desde parameters.py
+    E, S, TH, TD, TS, CO_s, CD_es, CH_e, TR_es, EP_es = PARAMETERS
 
-  model = gp.Model("Proyecto grupo 72")
-  model.setParam("TimeLimit", 1800)  # tiempo max de ejecución 30 minutos
+    model = gp.Model("Proyecto grupo 72")
+    model.setParam("TimeLimit", 1800)  # tiempo max de ejecución 30 minutos
 
-  # Variables
+    # Variables
 
-  # Restricciones
+    # Restricciones
 
-  # Función objetivo
+    # Función objetivo
 
-  # Optimizar
-  model.optimize
+    # Optimizar
+    model.optimize
 
-  # Conformación del archivo solución si es factible/óptimo
+    # Conformación del archivo solución si es factible/óptimo
 
-  return None
+    return None
 
 
 
