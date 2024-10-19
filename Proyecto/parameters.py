4461e9dce6f61ad4da594f7741d9361ef4d7ce5c
Feskenazi <100655059+Feskenazi@users.noreply.github.com>
2024-10-18 22:07:07 -0300

    Update parameters.py

--- Proyecto/parameters.py
+++ Proyecto/parameters.py
@@ -13,8 +13,6 @@ def get_data(csv_type):
       'source/small/costos_por_despliegue.csv', header=None)
     cos_hor = pd.read_csv(
       'source/small/costos_por_hora.csv', header=None)
-    des_sit = pd.read_csv(
-      'source/small/desastre_del_sitio.csv', header=None)
     hor_arreg = pd.read_csv(
       'source/small/horas_arreglo.csv', header=None)
     pue_encarg = pd.read_csv(
@@ -29,16 +27,15 @@ def get_data(csv_type):
   
   # Crear conjuntos
   
-  E = list(range(len(cos_despl)))
-  S = list(range(len(des_sit)))
-  #D = list(range(len(des_sit.columns)))
+  E = list(range(len(cos_despl.columns)))
+  S = list(range(len(cos_despl)))
   #TH = list(range(24))
-  #TD = list(range(7))
-  #TS = list(range(4))
+  TD = list(range(7))
+  TS = list(range(4))
   
   # Crear parámetros
 
-  #CO_ed = {(i, j): int(cos_opor.iat[i, j]) for i in E for j in D}
+  CO_s = {(i, j): int(.iat[i, j]) for i in E for j in }
   #CD_ed = {(i, j): int(cos_despl.iat[i, j]) for i in E for j in D}
   #CH ver que onda 
   #SD_sd = {(i, j): int(des_sit.iat[i, j]) for i in S for j in D}
