4bf2f6949765459d9b18a53aaf798c4e5cfcf3ee
Feskenazi <100655059+Feskenazi@users.noreply.github.com>
2024-10-19 22:39:31 -0300

    Update model.py

--- Proyecto/model.py
+++ Proyecto/model.py
@@ -16,6 +16,15 @@ def generate_model(PARAMETERS):
 
     # Variables
 
+    X_e_s_th = model.addVars(E, S, TH, vtype=GRB.BINARY, name="X_e_s_th")
+    U_e_s_th = model.addVars(E, S, TH, vtype=GRB.BINARY, name="U_e_s_th")
+    TU_e_s_th = model.addVars(E, S, TH, vtype=GRB.INTEGER, name="TU_e_s_th")
+    TM_s = model.addVars(S, vtype=GRB.INTEGER, name="TM_s")
+    Z_e_td_ts = model.addVars(E, TD, TS, vtype=GRB.INTEGER, name="Z_e_td_ts")
+    HW_e_th = model.addVars(E, TH, vtype=GRB.INTEGER, name="HW_e_th")
+    HD_e_th = model.addVars(E, TH, vtype=GRB.INTEGER, name="HD_e_th")
+    
+
     # Restricciones
 
     # Función objetivo
