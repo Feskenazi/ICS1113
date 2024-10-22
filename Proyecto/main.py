7e8cf68a4e47b0e5175347712bdd0bd4d28e488d
Feskenazi <100655059+Feskenazi@users.noreply.github.com>
2024-10-22 20:02:34 -0300

    Update main.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -5,7 +5,7 @@ from model import generate_model
 if __name__ == '__main__':
     
     print("Proyecto grupo 72")
-    print("Elegir la base de datos a utilizar: small, ..., real") # 2, 3 o 4 bases de datos
+    print("Elegir la base de datos a utilizar: small o real") # 2, 3 o 4 bases de datos
     
     base_datos = input("Introducir la base de datos que se quiere utilizar: ")
     
